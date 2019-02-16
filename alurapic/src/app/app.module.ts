35930e2661c1690fd40da48591194a21b3455ca1
Júnior <junior.jaava@gmail.com>
2019-02-16 17:30:55 +0000

    handle/importing different modules

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
-import { PhotoComponent } from './photo/photo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PhotoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
