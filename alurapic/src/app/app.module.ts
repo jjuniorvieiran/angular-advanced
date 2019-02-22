2c945df328084e6cae72c25032fb8cc09171b606
Júnior <junior.jaava@gmail.com>
2019-02-22 19:33:58 +0000

    working with inexistent routing

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     PhotosModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
