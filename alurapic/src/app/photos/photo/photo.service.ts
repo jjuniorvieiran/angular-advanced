45a24635ed62bf5b8aead8ee1f6ff11519a1a8bf
Júnior <junior.jaava@gmail.com>
2019-02-23 17:23:39 +0000

    handle valeus from route. getting values from route

--- alurapic/src/app/photos/photo/photo.service.ts
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -7,10 +7,10 @@ const API = 'http://localhost:3000';
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     listFromUser(userName: string) {
         return this.http
-            .get<Photo[]>(API + '/flavio/photos');
+            .get<Photo[]>(API + '/' + userName + '/photos');
     }
 }
